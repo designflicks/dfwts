985ad107046c37276bafdc9f1551741cc0222ae7
designflicks <designflicks@gmail.com>
2014-04-19 20:20:16 +0530

    tango

---
+++ reddit.js
@@ -0,0 +1,27 @@
+
+$("head").append("<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>");
+$(".link .title").css({fontFamily: 'Montserrat'});
+$(".link .title").css({fontWeight: 'bold', textTransform: 'uppercase'});
+$(".link .title").css({lineHeight: '1.5'});
+
+// width
+$(".link").css({width: '700px'});
+$(".link").css({padding: '25px'});
+
+$(".link").css({borderRadius: '0', borderBottom: '1px solid #d8d8d8', marginBottom: '0'});
+$(".link").css({marginLeft: '100px'});
+
+$(".link .title").css({fontSize: '14px'});
+$(".link").css({padding: '15px'});
+
+$(".link .midcol").css({marginLeft: '0', marginRight: '14px', marginTop: '3px'});
+$(".thumbnail").css({marginTop: '5px'});
+
+// sidebar
+$(".side").css({margin: "88px 196px 0px 0px"})
+$(".side").css({background: "transparent", boxShadow: 'none'})
+$(".side").css({margin: "88px 206px 0px 0px"})
+$(".md p").css({fontSize: '13px'});
+$(".md p").css({lineHeight: '1.5'});
+$(".md ol, .md ul").css({margin: '10px 0px 10px 20px'});
+$(".titlebox h1 a").css({top: '310px'})
