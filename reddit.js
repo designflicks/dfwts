3368809a855c9e20627b099b1a563fb45d829411
designflicks <designflicks@gmail.com>
2014-04-21 19:39:26 +0530

    reddit updates

--- reddit.js
+++ reddit.js
@@ -5,6 +5,7 @@ $(".link .title").css({fontWeight: 'bold', textTransform: 'uppercase'});
 $(".link .title").css({lineHeight: '1.5'});
 
 // width
+$(".content").css({marginLeft: '0'});
 $("#siteTable").css({width: '700px', marginLeft: '100px'})
 $(".link").css({padding: '25px', width: '100%'});
 $(".infobar.welcome").css({padding: '0 15px'});
@@ -45,3 +46,37 @@ $(".pagename a").css({display: 'none'});
 $(".titlebox h1 a").css({fontFamily: 'pacifico', color: 'white', fontWeight: 'normal', fontSize: '51px'})
 //testing
 $(".titlebox h1 a").css({top: '210px'})
+
+$("head").append("<style>#header:after {display: none;}</style>");
+$("#header .tabmenu").css({background: 'transparent', border: '0 none'})
+$(".tabmenu li a").css({fontFamily: 'Montserrat', color: 'white'})
+$(".content").css({marginTop: '0'})
+$(".titlebox h6:nth-of-type(1)").css({left: '100px', top: '190px', color: 'white'})
+$("#header .tabmenu").css({textAlign: 'left', paddingLeft: '630px'})
+
+
+
+
+$("head").append("<style>.footer:before {display: none;}</style>");
+
+$(".footer a").css({color: 'white'})
+$(".footer a").css({fontFamily: 'Montserrat'})
+$(".footer a").css({fontSize: '12px', lineHeight: '1.75'})
+
+axisbank$(".footer .col").css({border: '0 none'})
+
+$(".footer-parent").css({textAlign: 'left'})
+
+$(".footer li.title").css({textTransform: 'uppercase'})
+$(".footer li.title").css({fontFamily: 'Montserrat'})
+$(".footer li.title").css({color: 'wheat'})
+$(".footer li.title").css({fontSize: '14px', fontWeight: 'bold'})
+$(".footer li.title").css({marginBottom: '15px'})
+
+$(".footer-parent").css({marginTop: '30px'});
+$(".footer .col").css({padding: '0', marginRight: '100px', height: 'auto'})
+$(".footer").css({margin: '0', padding: '0', marginLeft: '100px'});
+
+$(".bottommenu").css({marginLeft: '100px', color: 'white'})
+$(".bottommenu a").css({color: 'white'})
+$(".footer").css({marginBottom: '20px'});
