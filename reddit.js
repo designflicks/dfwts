f597219abd0c710969b5872924c1abf838c32c47
designflicks <designflicks@gmail.com>
2014-04-22 13:54:25 +0530

    reddit updates

--- reddit.js
+++ reddit.js
@@ -3,7 +3,8 @@
 
 $("head").append("<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Pacifico' rel='stylesheet' type='text/css'>");
 $(".link .title").css({fontFamily: 'Montserrat'});
-$(".link .title").css({fontWeight: 'bold', textTransform: 'uppercase'});
+//$(".link .title").css({fontWeight: 'bold', textTransform: 'uppercase'});
+$(".link .title").css({fontWeight: 'bold', color: '#333'});
 $(".link .title").css({lineHeight: '1.5'});
 
 // width
@@ -13,7 +14,7 @@ $(".link").css({padding: '25px', width: '100%'});
 $(".infobar.welcome").css({padding: '0 15px'});
 $(".link, .infobar.welcome").css({boxSizing: 'border-box'});
 
-$(".link").css({borderRadius: '0', borderBottom: '1px solid #d8d8d8', marginBottom: '0'});
+$(".link").css({borderRadius: '0', borderBottom: '1px solid #e6e6e6', marginBottom: '0'});
 
 $(".link .title").css({fontSize: '14px'});
 $(".link").css({padding: '15px'});
@@ -52,6 +53,7 @@ $(".titlebox h1 a").css({top: '210px'})
 $("head").append("<style>#header:after {display: none;}</style>");
 $("#header .tabmenu").css({background: 'transparent', border: '0 none'})
 $(".tabmenu li a").css({fontFamily: 'Montserrat', color: 'white'})
+$(".tabmenu li a").css({margin: '0px 30px 0px 0'})
 $(".content").css({marginTop: '0'})
 $(".titlebox h6:nth-of-type(1)").css({left: '100px', top: '190px', color: 'white'})
 $("#header .tabmenu").css({textAlign: 'left', paddingLeft: '630px'})
@@ -82,3 +84,34 @@ $(".footer").css({margin: '0', padding: '0', marginLeft: '100px'});
 $(".bottommenu").css({marginLeft: '100px', color: 'white'})
 $(".bottommenu a").css({color: 'white'})
 $(".footer").css({marginBottom: '20px'});
+
+
+$(".side .spacer:nth-child(3), .side .spacer:nth-child(5)").css({display: 'none'});
+$(".side .spacer").css({marginBottom: '30px', marginTop: '0'});
+$(".side .spacer").css({background: 'rgba(255, 255, 255, 1)'});
+$(".side").css({margin: "88px 218px 0px 0px"})
+
+$("#search input[type=text]").css({marginTop: '0'})
+$(".sidecontentbox .content li").css({marginBottom: '8px'})
+$(".side").css({paddingTop: '0'});
+$(".side .spacer:first-child").css({background: 'transparent'})
+$("#search input[type=text]").css({borderRadius: '3px', border: '1px solid white', backgroundColor: 'transparent'})
+
+$(".side").css({background: 'rgba(255, 255, 255, 0.9)'})
+$(".side .spacer").css({background: 'transparent'})
+$(".side").css({borderRadius: '0'})
+$(".side").css({width: '330px'});
+
+$("#siteTable").css({position: 'relative', boxShadow: '0 0 15px 0 rgba(0, 0, 0, 0.25)'})
+$(".link").css({borderLeft: '0 none', borderRight: '0 none'})
+
+$("a.author").css({color: '#999', borderBottom: '1px dotted #aaa', display: 'inline-block'});
+$(".link .entry li .comments").css({color: '#999', borderBottom: '1px dotted #aaa', fontWeight: 'normal'})
+$(".link .flat-list").css({padding: '0'})
+$(".link .tagline, .link .entry li .comments").css({float: 'left', marginRight: '5px', fontSize: '11px', lineHeight: '1.6em', padding: '0'});
+
+$("#header").css({height: '150px'});
+$(".titlebox h6:nth-of-type(1)").css({top: '125px', fontFamily: 'Montserrat'})
+$("body").css({backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(http://24.media.tumblr.com/56a9757318c0f2ed1c1c687f254b2fb8/tumblr_mopqkkwK2M1st5lhmo1_1280.jpg)'})
+$(".titlebox h1 a").css({top: '145px'})
+$("#header .tabmenu").css({top: '112px'})
