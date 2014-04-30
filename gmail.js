086d01fceb75d5c03482abe828c9ac1a5fc04cc9
designflicks <designflicks@gmail.com>
2014-04-30 22:02:17 +0530

    tma started

---
+++ gmail.js
@@ -0,0 +1,2 @@
+$("head").append("<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Pacifico' rel='stylesheet' type='text/css'>");
+$("head").append('<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">');
---
+++ pma.js
@@ -0,0 +1,15 @@
+$("head").append("<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700|Pacifico' rel='stylesheet' type='text/css'>");
+$("head").append('<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">');
+
+$("#topmenucontainer").css({background: 'transparent'})
+$("#topmenu").css({overflow: 'hidden'})
+$("#topmenu li").css({border: '0 none'})
+$("#topmenu a").css({fontSize: '12px'})
+
+$(".data a").css({fontSize: '12px'})
+$(".data thead th").css({background: 'transparent', borderBottom: '1px solid #ccc'})
+
+$(".data td:nth-child(3) a, .data td:nth-child(4) a, .data td:nth-child(5) a, .data td:nth-child(6) a, .data td:nth-child(7) a, .data td:nth-child(8) a").hide();
+
+$("#page_content").css({margin: '0'})
+$(".data").css({width: '100%'})
